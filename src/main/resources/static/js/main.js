e0ddd196a82b64b009626c599895b7e1d665cb98
yesolje <70881757+yesolje@users.noreply.github.com>
2023-01-07 20:07:01 +0900

    메뉴구성 분리 및 지역 셀렉트박스 생성

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,17 +5,31 @@
 /********************************************************************************
  * Global Variable : 전역변수 정의
  ********************************************************************************/
+ var sampledata = [ //자료 불러오는 대로 삭제할 예정
+ 	{id:1, name:"AAA주유소", location:"경기도 성남시 분당구 동판교로 212", priceGasoline:"1780", priceDiesel:"1230"},
+ 	{id:2, name:"BBB주유소", location:"경기도 성남시 분당구 동판교로 212", priceGasoline:"1780", priceDiesel:"1230"},
+ 	{id:3, name:"CCC주유소", location:"경기도 성남시 분당구 동판교로 212", priceGasoline:"1780", priceDiesel:"1230"},
+ 	{id:4, name:"DDD주유소", location:"경기도 성남시 분당구 동판교로 212", priceGasoline:"1780", priceDiesel:"1230"},
+ 	{id:5, name:"EEE주유소", location:"경기도 성남시 분당구 동판교로 212", priceGasoline:"1780", priceDiesel:"1230"},
+ ];
 
 /********************************************************************************
  * Document Ready
  ********************************************************************************/
 
 document.addEventListener("DOMContentLoaded", function(){
+    main.initHeaderLoad();
     main.initEvent();
-
+    main.initMapLoad();
+    main.initTabulatorLoad();
+    common.loadLocationSelectBox();
 });
 
 var main={
+    initHeaderLoad : function(){
+        common.headerLoad();
+        common.navLoad();
+    },
     initEvent:function(){
         //document.getElementById('login').addEventListener("click", function(event){
         //  document.getElementById('loginForm').submit();
@@ -23,8 +37,32 @@ var main={
         // document.getElementById('join').addEventListener("click", function(event){
         //          document.getElementById('joinForm').submit();
         // });
+        document.getElementById('searchSido').addEventListener("change", function(event){
+            //alert(document.getElementById('searchSido').value);
+            common.loadLocationSelectBox(document.getElementById('searchSido').value);
+        });
     },
+    initMapLoad:function(){
+        let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+        let options = { //지도를 생성할 때 필요한 기본 옵션
+        	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        	level: 3 //지도의 레벨(확대, 축소 정도)
+        };
 
+        let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    },
+    initTabulatorLoad:function(){
+        var table = new Tabulator("#listTabulator", {
+         	data:sampledata, //assign data to table
+         	layout:"fitColumns", //fit columns to width of table (optional)
+         	columns:[ //Define Table Columns
+        	 	{title:"주유소명", field:"name", width: "20%"},
+        	 	{title:"위치", field:"location"},
+        	 	{title:"휘발유", field:"priceGasoline", width: "15%"},
+        	 	{title:"경유", field:"priceDiesel", width: "15%"},
+         	],
+        });
+    },
 
 
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    //
-// Scripts
-//
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
@@ -24,3 +15,139 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
 });
+/********************************************************************************
+ * common js 설정
+ ********************************************************************************/
+const common = {
+    //headerLoad : 세션의 유무에 따라 다른 header 를 html load 해온다.
+    headerLoad: function(){
+        let info = document.querySelector('#info').value;
+        let header = document.querySelector('#headerMenu');
+        header.innerHTML = "";
+        if(info == 'none'){
+            header.innerHTML =
+                 "<a class='navbar-brand ps-3 logo' href='/main'>만땅!</a>"
+                +"<button class='btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0' id='sidebarToggle' href='#!'><i class='fas fa-bars'></i></button>"
+                +"<form class='d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0'>"
+                +       "<div class='input-group'>"
+                +          "<input class='form-control' type='text' placeholder='주유소명 검색' aria-label='Search for...' aria-describedby='btnNavbarSearch' />"
+                +           "<button class='btn btn-primary' id='btnNavbarSearch' type='button'><i class='fas fa-search'></i></button>"
+                +       "</div>"
+                +"</form>"
+                +"<ul class='navbar-nav ms-auto ms-md-0 me-3 me-lg-4'>"
+                +   "<li class='nav-item dropdown'>"
+                +       "<a class='header-message' id='login' href='/login'>로그인  </a>"
+                +       "<a class='header-message' id='join' href='/join'> 회원가입</a>"
+                +   "</li>"
+                +"</ul>"
+        }else{
+            header.innerHTML =
+                 "<a class='navbar-brand ps-3 logo' href='/main/user'>만땅!</a>"
+                +"<button class='btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0' id='sidebarToggle' href='#!'><i class='fas fa-bars'></i></button>"
+                +"<form class='d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0'>"
+                +       "<div class='input-group'>"
+                +          "<input class='form-control' type='text' placeholder='주유소명 검색' aria-label='Search for...' aria-describedby='btnNavbarSearch' />"
+                +           "<button class='btn btn-primary' id='btnNavbarSearch' type='button'><i class='fas fa-search'></i></button>"
+                +       "</div>"
+                +"</form>"
+                +"<ul class='navbar-nav ms-auto ms-md-0 me-3 me-lg-4'>"
+                +   "<li class='nav-item dropdown'>"
+                +       "<a class='nav-link dropdown-toggle' id='navbarDropdown' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'><i class='fas fa-user fa-fw'></i></a>"
+                +       "<ul class='dropdown-menu dropdown-menu-end' aria-labelledby='navbarDropdown'>"
+                +       "<li><a class='dropdown-item dropdown-name'>"+info+"님</a></li>"
+                +       "<li><a class='dropdown-item' href='#!'>계정정보변경</a></li>"
+                +       "<li><a class='dropdown-item' href='#!'>가격알림보기</a></li>"
+                +       "<li><hr class='dropdown-divider' /></li>"
+                +       "<li>"
+                +       "<form method='post' action='/logout'>"
+                +           "<button type='submit' class='dropdown-item dropdown-warning'>로그아웃</button>"
+                +       "</form>"
+                +   "</li>"
+                +"</ul>"
+        }
+
+    },
+
+    //navLoad : 세션의 유무에 따라 다른 nav 를 html load 해온다.
+    navLoad: function(){
+        let info = document.querySelector('#info').value;
+        let nav = document.querySelector('#sidenavAccordion');
+        nav.innerHTML = "";
+        if(info == 'none'){
+            nav.innerHTML =
+                "<div class='sb-sidenav-menu'>"
+                +   "<div class='nav'>"
+                +       "<div class='sb-sidenav-menu-heading'>유가 정보</div>"
+                +       "<a class='nav-link' href='/main'>"
+                +           "<div class='sb-nav-link-icon'><i class='fas fa-tachometer-alt'></i></div>유가확인"
+                +       "</a>"
+                +       "<a class='nav-link' href='index.html'>"
+                +           "<div class='sb-nav-link-icon'><i class='fas fa-tachometer-alt'></i></div>유가통계"
+                +       "</a>"
+                +   "</div>"
+                +"</div>"
+                +"<div class='sb-sidenav-footer'>"
+                +   "<div class='small'>Logged in as:</div>Start Bootstrap"
+                +"</div>"
+        }else{
+            nav.innerHTML =
+                "<div class='sb-sidenav-menu'>"
+                +   "<div class='nav'>"
+                +       "<div class='sb-sidenav-menu-heading'>유가 정보</div>"
+                +       "<a class='nav-link' href='/main/user'>"
+                +           "<div class='sb-nav-link-icon'><i class='fas fa-tachometer-alt'></i></div>유가확인"
+                +       "</a>"
+                +       "<a class='nav-link' href='index.html'>"
+                +           "<div class='sb-nav-link-icon'><i class='fas fa-tachometer-alt'></i></div>유가통계"
+                +       "</a>"
+                +       "<a class='nav-link' href='index.html'>"
+                +           "<div class='sb-nav-link-icon'><i class='fas fa-tachometer-alt'></i></div>찜목록"
+                +       "</a>"
+                +   "</div>"
+                +"</div>"
+                +"<div class='sb-sidenav-footer'>"
+                +   "<div class='small'>Logged in as:</div>Start Bootstrap"
+                +"</div>"
+        }
+
+    },
+    loadLocationSelectBox:function(sido){
+        if(sido =='' ||sido == null ||sido == undefined){
+            //초기 박스 로드시 sido 채우지 않은채 전달
+            //sido 없을 시 big location에는 전체, small location에는 "==시도를 먼저 선택해 주세요=="
+            var searchSido = document.querySelector("#searchSido");
+            var searchGungu = document.querySelector("#searchGungu");
+
+            var sidoOption = document.createElement("option");
+            var gunguOption = document.createElement("option");
+
+            for(var i=0;i<LOCATION_DATA.length;i++){
+                sidoOption = document.createElement("option");
+                sidoOption.text = LOCATION_DATA[i].sido;
+                sidoOption.value = LOCATION_DATA[i].sido;
+                searchSido.options.add(sidoOption);
+            };
+            searchSido[0].selected = true;
+            gunguOption.text = "전체";
+            gunguOption.value = "";
+            searchGungu.options.add(gunguOption);
+            searchGungu[0].selected = true;
+        }else{
+            console.log(sido);
+            var searchGungu = document.querySelector("#searchGungu");
+            var gunguOption = document.createElement("option");
+            searchGungu.options.length=0;
+            for(var i=0;i<LOCATION_DATA.length;i++){
+                if(LOCATION_DATA[i].sido == sido){
+                    for(var j=0;j<LOCATION_DATA[i].gungu.length;j++){
+                        gunguOption = document.createElement("option");
+                        gunguOption.text = LOCATION_DATA[i].gungu[j];
+                        gunguOption.value = LOCATION_DATA[i].gungu[j];
+                        searchGungu.options.add(gunguOption);
+                    };
+                }
+            }
+        }
+    },
+
+}
