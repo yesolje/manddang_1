24cb8042f96d0f116d39b8ff007080b01fafb5b5
yesolje <70881757+yesolje@users.noreply.github.com>
2023-02-07 16:18:54 +0900

    api 서버단 통신 성공

--- src/main/java/com/yesol/manddang_1/controller/AreaController.java
+++ src/main/java/com/yesol/manddang_1/controller/AreaController.java
@@ -2,13 +2,18 @@ package com.yesol.manddang_1.controller;
 
 import com.yesol.manddang_1.service.AreaService;
 import com.yesol.manddang_1.vo.Area;
-import com.yesol.manddang_1.vo.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +29,50 @@ public class AreaController {
         return res;
     }
 
+    //지역코드호출(테스트용)
+    @RequestMapping(value = "/manage/getArea", produces ="application/json")
+    public ResponseEntity<?> getArea(@RequestBody String sido_cd) throws IOException {
+        System.out.println("@@@@@@"+sido_cd);
+        StringBuffer result = new StringBuffer();
+        String strResult = "";
+        String code="F211229279";
+        try {
+            // URL 설정
+            StringBuilder urlBuilder = new StringBuilder("http://www.opinet.co.kr/api/areaCode.do");
+
+            // search 변수는 인코딩이 필요하다고 했으므로 그 부분만 인코딩
+            urlBuilder.append("?out=json");
+            urlBuilder.append("&code=" + code);
+            urlBuilder.append("&area="+sido_cd);
+
+            URL url = new URL(urlBuilder.toString());
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            // Request 형식 설정
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            // 응답 데이터 받아오기
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 & conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+
+            String line;
+            while((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            strResult = result.toString();
+        } catch ( Exception e ){
+            e.printStackTrace();
+        }
+        ResponseEntity res = new ResponseEntity<>(strResult, HttpStatus.OK);
+        return res;
+    }
 
 
     
