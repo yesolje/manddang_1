09730be7b666fd75a267d738de962fb850d6b173
yesolje <70881757+yesolje@users.noreply.github.com>
2023-04-11 16:02:54 +0900

    public 삭제

--- src/main/java/com/yesol/manddang_1/repository/AreaRepository.java
+++ src/main/java/com/yesol/manddang_1/repository/AreaRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface AreaRepository extends JpaRepository<Area,Long> {
     @Query(nativeQuery = true, value="SELECT a.sido_cd from manddang.area a where a.sido_nm = :sido_nm limit 1")
-    public String selectSidoCdBySidoNm(@Param("sido_nm") String sido_nm);
+    String selectSidoCdBySidoNm(@Param("sido_nm") String sido_nm);
 
     @Query(nativeQuery = true, value="SELECT a.area_cd from manddang.area a where a.sido_cd = :sido_cd and a.area_nm = :area_nm")
-    public String selectAreaCdByAreaNm(@Param("sido_cd") String sido_cd , @Param("area_nm") String area_nm);
+    String selectAreaCdByAreaNm(@Param("sido_cd") String sido_cd , @Param("area_nm") String area_nm);
 }
