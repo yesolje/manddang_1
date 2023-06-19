72b09e7b54ca82103e0f8781dc001167383e940c
yesolje <70881757+yesolje@users.noreply.github.com>
2023-06-19 19:26:31 +0900

    주유소 리스트 클릭시 지도이벤트

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -8,13 +8,23 @@
  //새로고침할때마다 업데이트되는 유가 데이터
  var oilPriceData;
  var table;
+
+ //카카오맵
+ var map;
+
+ // proj4js 라이브러리 불러오기
+ // Katec 좌표
+    proj4.defs("EPSG:5181", "+proj=tmerc +lat_0=38 +lon_0=128 +k=0.9999 +x_0=400000 +y_0=600000 +ellps=bessel +units=m +no_defs");
+    proj4.defs("EPSG:4326", "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs");
+
+
 /********************************************************************************
  * Document Ready
  ********************************************************************************/
 
 document.addEventListener("DOMContentLoaded", async function(){
     main.initHeaderLoad();
-    main.initEvent();
+
     main.initMapLoad();
     common.loadLocationSelectBox();
 
@@ -22,7 +32,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     oilPriceData = await common.getOilPriceFetch('전체','','B027');
     oilPriceData = oilPriceData.RESULT.OIL;
     main.initTabulatorLoad();
-
+    main.initEvent();
 });
 
 var main={
@@ -45,16 +55,23 @@ var main={
                 table.replaceData(oilPriceData.RESULT.OIL);
             });
         });
+        table.on("rowClick", function(e, row){
+            console.log(row.getData().GIS_X_COOR);
+            console.log(row.getData().GIS_Y_COOR);
+            var latlng = main.alterKatecToWgs(row.getData().GIS_X_COOR,row.getData().GIS_Y_COOR);
+            main.setSenter(latlng[1],latlng[0]);
+
+        });
     },
 
     initMapLoad:function(){
         let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         let options = { //지도를 생성할 때 필요한 기본 옵션
-        	center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+        	center: new kakao.maps.LatLng(37.413293, 127.143591), //지도의 중심좌표.
         	level: 3 //지도의 레벨(확대, 축소 정도)
         };
 
-        let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
     },
     initTabulatorLoad:function(){
         table = new Tabulator("#listTabulator", {
@@ -75,6 +92,25 @@ var main={
         });
     },
 
+    setSenter:function(lat,lng){
+    console.log("여기까지 들어왔어");
+    console.log(lat);
+    console.log(lng);
+    var moveLatLon = new kakao.maps.LatLng(lat,lng);
+    map.setCenter(moveLatLon);
+    var marker = new kakao.maps.Marker({
+        position: moveLatLon
+    });
+    marker.setMap(map);
+    },
+
+    alterKatecToWgs:function(x,y){
+        var katecArr = [];
+        katecArr[0] = x;
+        katecArr[1] = y;
+        var wgs84Coords = proj4("EPSG:5181", "EPSG:4326", katecArr);
+        return wgs84Coords;
+    },
 
 }
 
